af82e87216e3f3d27fd78b0d19ce75dd55f48ee2
Olga RG <olga_1847@hotmail.com>
2022-09-20 00:25:10 +0200

    Add RRSS to contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,65 @@
-function Contact () {
-    return (
-        <h1>Contact</h1>
-    )
+import "../styles/layout/contact.scss";
+import pdf from "../images/cv-olgargarrucho.pdf";
+
+function Contact() {
+  return (
+    <div className="contact">
+      <h2 className="contact__title">Contact</h2>
+      <section className="contact__rrss">
+        <a
+          href="https://github.com/olgargarrucho"
+          className="contact__rrss--link"
+          target="_blank"
+          rel="noreferrer"
+          title="Go to github"
+        >
+          <i className="icon fab fa-github-square"></i>
+          Github
+        </a>
+        <a
+          href="https://www.linkedin.com/in/olgargarrucho/"
+          className="contact__rrss--link"
+          target="_blank"
+          rel="noreferrer"
+          title="Go to linkedin"
+        >
+          <i className="icon fab fa-linkedin"></i>
+          Linkedin
+        </a>
+        <a
+          href="https://twitter.com/olgargarrucho"
+          className="contact__rrss--link"
+          target="_blank"
+          rel="noreferrer"
+          title="Go to twitter"
+        >
+          <i className="icon fab fa-twitter-square"></i>
+          Twitter
+        </a>
+        <a
+          href="mailto: olga_1847@hotmail.com"
+          className="contact__rrss--link"
+          target="_blank"
+          rel="noreferrer"
+          title="Send an email"
+        >
+          <i className="fa-solid fa-envelope"></i>
+          Email
+        </a>
+        <a
+          href={pdf}
+          className="contact__rrss--link"
+          target="_blank"
+          rel="noreferrer"
+          download="cv-olgargarrucho.pdf"
+          title="Download cv"
+        >
+          <i class="icon fa-solid fa-file-arrow-down"></i>
+          Download my CV
+        </a>
+      </section>
+    </div>
+  );
 }
 
 export default Contact;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,18 +3,7 @@ import '../styles/layout/footer.scss';
 function Footer() {
     return (
       <footer className='footer'>
-      <small className='footer__copy'>&copy; 2022 Olga RG</small>
-      <section className='footer__rrss'>
-        <a href='https://github.com/olguita2412' className='footer__rrss--link' target='_blank' rel="noreferrer">
-          <i className='icon fab fa-github-square'></i>
-        </a>
-        <a href='https://www.linkedin.com/in/olguita2412/' className='footer__rrss--link' target='_blank' rel="noreferrer">
-          <i className='icon fab fa-linkedin'></i>
-        </a>
-        <a href='https://twitter.com/olguiita24' className='footer__rrss--link' target='_blank' rel="noreferrer">
-          <i className='icon fab fa-twitter-square'></i>
-        </a>
-      </section>
+      <small className='footer__copy'>&copy; 2022 Olga R. Garrucho</small>
     </footer>
     );
   }
