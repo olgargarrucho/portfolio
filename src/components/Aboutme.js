4137eeddf6b1bb184804891b5b5c95b1e8301ab1
Olga RG <olga_1847@hotmail.com>
2022-09-20 00:45:21 +0200

    Add about me section

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,7 +1,45 @@
-function Aboutme () {
-    return (
-        <h1>About me</h1>
-    )
+import "../styles/layout/aboutme.scss";
+
+function Aboutme() {
+  return (
+    <div className="aboutme">
+      <h2 className="aboutme__title">About me</h2>
+      <h3 className="aboutme__subtitle">Hi everyone!</h3>
+      <p className="aboutme__text">
+        My name is Olga and I from Spain. I was born and grown up in Cádiz, but
+        now I live in Madrid. I'm a creative and very curious person who can't
+        stop looking for challenges and doing them. I like learning new things
+        and that's why now I have decided to start a new path in technology
+        trhough web programming 👩‍💻. I loved it 💖.
+      </p>
+      <p className="aboutme__text">
+        I'm looking for my first opportunity as a front-end developer. I would
+        love to continue learning more about it.
+      </p>
+      <h3 className="aboutme__subtitle">I’m currently learning:</h3>
+      <p className="aboutme__text">
+        - Testing course 🙇‍♀️
+      </p>
+      <p className="aboutme__text">
+        - iOs app and Swift language 📱
+      </p>
+      <p className="aboutme__text">
+        - English language 🇬🇧
+      </p>
+      <h3 className="aboutme__subtitle">Fun facts:</h3>
+      <p className="aboutme__text">
+        - I just finished a front-end developer bootcamp at Adalab 💻 
+      </p>
+      <p className="aboutme__text">- I love the animals! 🐾</p>
+      <p className="aboutme__text">
+        - I'm addicted to DIY, especially the sewing! 🪡
+      </p>
+      <p className="aboutme__text">- My zodiac sign is Capricorn ♑</p>
+      <p className="aboutme__text">
+        - I have a degree in Edification Engineering! 👷
+      </p>
+    </div>
+  );
 }
 
 export default Aboutme;
