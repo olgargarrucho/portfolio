cf9590c79140e6a82580c1cee6fee969fdeec993
Olga RG <olga_1847@hotmail.com>
2022-09-20 00:56:09 +0200

    Add home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
-function Home () {
-    return (
-        <h1>Home</h1>
-    )
+import "../styles/layout/home.scss";
+import photo from "../images/photo.jpeg"
+
+function Home() {
+  return (
+    <div className="home">
+        <div className="home__texts">
+      <h2 className="home__texts-subtitle">Hi! I'm</h2>
+      <h1 className="home__texts-title">Olga R. Garrucho</h1>
+      </div>
+      <img src={photo} className="home__img" />
+    </div>
+  );
 }
 
 export default Home;
